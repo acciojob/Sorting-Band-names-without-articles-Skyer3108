39640db7ee51839fa32bc8ba8ac75d29f113dada
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-11 20:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+function sortBandNames(arr) {
+
+	function removeArticles(name){
+		return name.replace(/^(?:The|An?|A)\s+/i, '')
+
+	}
+
+	arr.sort((a,b)=>{
+		let nameA=removeArticles(a);
+		let nameB=removeArticles(b)
+		return nameA.localeCompare(nameB)
+	})
+
+	let ul=doument.getElementById('band')
+
+	ul.innerHTML=''
+
+	arr.forEach((name)=>{
+
+		let li=document.createElement('li');
+		li.textContent=name;
+		ul.appendChild(li)
+		
+	})
+}
+
+sortBandNames(touristSpots)
